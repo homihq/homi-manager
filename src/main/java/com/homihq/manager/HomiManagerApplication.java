af567c9b2040eb35db2b77c7aace84980478d0db
grabdoc <grabdoc2020@gmail.com>
2022-11-13 00:17:49 -0600

    adding new screen

---
+++ src/main/java/com/homihq/manager/HomiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.homihq.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomiManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomiManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homihq/manager/HomiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homihq.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomiManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
